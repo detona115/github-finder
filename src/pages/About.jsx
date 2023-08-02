98c8e5d69d7a81bf77c9a0991bbda1587bf64d11
ANDY <andykiaka@gmail.com>
2023-08-02 04:48:07 -0400

    added 2 pages and their respective routes in App.js alongside with some layout in About and NotFound pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,21 @@
-
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search Github profiles and see profile detail.
+        <br />
+        Implemented by
+        <strong>
+          {' '}
+          <a href='https://www.linkedin.com/in/andy-k-76a983110/'>ANDY KIAKA</a>
+        </strong>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
