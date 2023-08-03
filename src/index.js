b041858e342c2893e2173cff8a7876c24a481e22
ANDY <andykiaka@gmail.com>
2023-08-03 02:55:39 -0400

    addedGithubContext and updated readme.md

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { GithubProvider } from './context/github/GithubContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GithubProvider>
+        <App />
+      </GithubProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
