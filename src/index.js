b70e3526b0b1c902e5c908c55032fb259045a302
ANDY <andykiaka@gmail.com>
2023-08-07 01:53:32 -0400

    added alert component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <GithubProvider>
-        <App />
+        <AlertProvider>
+          <App />
+        </AlertProvider>
       </GithubProvider>
     </BrowserRouter>
   </React.StrictMode>
