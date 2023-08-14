970ee2aca7191601310164cbee6953dcb670c3ca
ANDY <andykiaka@gmail.com>
2023-08-13 23:39:56 -0400

    cleaned GithubActions User.jsx and GithubReducer

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import GithubContext from '../context/github/GithubContext';
 import Spinner from '../components/layout/Spinner';
 import RepoList from '../components/repos/RepoList';
-import { getUser, getUserRepos } from '../context/github/GithubActions';
+import { getUserAndRepos } from '../context/github/GithubActions';
 
 function User() {
   const { user, dispatch, repos, loading } = useContext(GithubContext);
@@ -14,16 +14,11 @@ function User() {
   useEffect(() => {
     dispatch({ type: 'SET_LOADING' });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
-      dispatch({ type: 'GET_USER', payload: userData });
-
-      const userRepos = await getUserRepos(params.login);
-      dispatch({ type: 'GET_REPOS', payload: userRepos });
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
     };
 
     getUserData();
-
-    
   }, [dispatch, params.login]);
 
   const {
