0a1ea0b9d2ef92bc213c467776f224ae03359bde
ANDY <andykiaka@gmail.com>
2023-08-13 23:14:44 -0400

    refactored and cleaned githubContext to use state and dispatch from GithubActions

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,9 +3,6 @@ import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   // const [users, setUsers] = useState([]);
   // const [loading, setLoading] = useState(true);
@@ -18,96 +15,11 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Clear results
-  const ClearResults = () => {
-    dispatch({
-      type: 'CLEAR_RESULTS',
-    });
-  };
-
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-    const params = new URLSearchParams({
-      q: text,
-    });
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const { items } = await response.json();
-    // setUsers(data);
-    // setLoading(false);
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
-  };
-
-  // Get a single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (response.status === 404) {
-      window.location = '/notfound';
-    } else {
-      const data = await response.json();
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      });
-    }
-  };
-
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10,
-    });
-
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-        headers: {
-          Authorization: `Bearer ${GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data,
-    });
-  };
-
-  const setLoading = () =>
-    dispatch({
-      type: 'SET_LOADING',
-    });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        loading: state.loading,
-        repos: state.repos,
-        searchUsers,
-        ClearResults,
-        getUser,
-        getUserRepos,
+        ...state,
+        dispatch,
       }}
     >
       {children}
